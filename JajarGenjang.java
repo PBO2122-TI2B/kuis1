bcd9d7022b9f954651878bb8d102bba8fa0cfce4
Ahmad Abid <91647810+ahmadabidb@users.noreply.github.com>
2021-09-30 22:04:33 +0700

    03_Ahmad Abid

---
+++ JajarGenjang.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+class JajarGenjang {
+    private int alas;
+    private int tinggi;
+    private int sisiM;
+    
+    public void setAlas(int a){
+        this.alas = a;
+    }
+    
+    public void setTinggi(int t){
+        this.tinggi = t;
+    }
+    
+    public void setSisi(int s){
+        this.sisiM = s;
+    }
+    
+    public int getLuas(){
+        int luas = alas * tinggi;
+        return luas;
+    }
+    
+    public int getKeliling(){
+        int keliling = 2 * (alas + sisiM);
+        return keliling;
+    }
+    
+    public void cetak(){
+        System.out.println("Alas        : "+alas);
+        System.out.println("Tinggi      : "+tinggi);
+        System.out.println("Sisi Miring : "+sisiM);
+    }
+}
---
+++ KuisMain.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class KuisMain {
+    public static void menu(){
+        System.out.println("Masukkan menu yang di inginkan:");
+        System.out.println("1. Hitung Segitiga");
+        System.out.println("2. Hitung Persegi");
+        System.out.println("3. Hitung Persegi Panjang");
+        System.out.println("4. Hitung Lingkaran");
+        System.out.println("5. Hitung Jajar Genjang");
+        System.out.println("6. Keluar");
+        System.out.println("----------------------------------");
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Segitiga sgt = new Segitiga();
+        Persegi p = new Persegi();
+        PersegiPanjang pp = new PersegiPanjang();
+        Lingkaran lkr = new Lingkaran();
+        JajarGenjang jg = new JajarGenjang();
+        
+        int Pilih;
+        do{
+            menu();
+            Pilih = sc.nextInt();
+            switch(Pilih){
+                case 1:
+                    System.out.println("Menghitung Segitiga");
+                    sgt.setAlas(8);
+                    sgt.setTinggi(6);
+                    sgt.getSisiM();
+                    sgt.cetak();
+                    System.out.println("Luas Segitiga : "+sgt.getLuas());
+                    System.out.println("Keliling Segitiga : "+sgt.getKeliling());
+                    break;
+                case 2: 
+                    System.out.println("Menghitung Persegi");
+                    p.setSisi(5);
+                    p.cetak();
+                    System.out.println("Luas Persegi : "+p.getLuas());
+                    System.out.println("Keliling Persegi : "+p.getKeliling());
+                    break;
+                case 3:
+                    System.out.println("Menghitung Persegi Panjang");
+                    pp.setPanjang(8);
+                    pp.setLebar(4);
+                    pp.cetak();
+                    System.out.println("Luas Persegi Panjang : "+pp.getLuas());
+                    System.out.println("Keliling Persegi Panjang : "+pp.getKeliling());
+                    break;
+                case 4:
+                    System.out.println("Menghitung Lingkaran");
+                    lkr.setR(7.5);
+                    lkr.setPhi(3.5);
+                    lkr.cetak();
+                    System.out.println("Luas Lingkaran : "+lkr.getLuas());
+                    System.out.println("Keliling Lingkaran : "+lkr.getKeliling());
+                    break;
+                case 5:
+                    System.out.println("Menghitung Jajar Genjang");
+                    jg.setAlas(7);
+                    jg.setTinggi(10);
+                    jg.setSisi(6);
+                    jg.cetak();
+                    System.out.println("Luas Jajar Genjang : "+jg.getLuas());
+                    System.out.println("Keliling Jajar Genjang : "+jg.getKeliling());
+                    break;
+            }
+        } while (Pilih != 6) ;
+    }
+}
+
